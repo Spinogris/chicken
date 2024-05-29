6fefbeb7706893b9a9e6e8d181876ffa57aa407a
Spinogris <vladislav77788@gmail.com>
2024-05-29 10:12:07 +0200

    @JsonInclude in AccountResponceDTO

--- src/main/java/com/chicken/de/demo/DTO/account/AccountResponceDTO.java
+++ src/main/java/com/chicken/de/demo/DTO/account/AccountResponceDTO.java
@@ -1,5 +1,6 @@
 package com.chicken.de.demo.DTO.account;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Setter
 public class AccountResponceDTO {
     public String firstName;
+    @JsonInclude(JsonInclude.Include.NON_NULL) //не показывает поле если оно равно NULL
     public String lastName;
     public String email;
     public String phoneNumber;
